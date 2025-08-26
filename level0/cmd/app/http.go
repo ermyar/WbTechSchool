ca42c6b232c2ee0818184e799cd8f75002dea935
Ermachkov Yaroslav <eryar.school@gmail.com>
2025-08-27 02:12:23 +0300

    shows perfomance of cache

--- level0/cmd/app/http.go
+++ level0/cmd/app/http.go
@@ -4,7 +4,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"html/template"
+	"log/slog"
 	"net/http"
+	"time"
 )
 
 var temp = template.Must(template.ParseGlob("templates/*.html"))
@@ -25,17 +27,23 @@ func getOrderHandler(w http.ResponseWriter, r *http.Request) {
 
 	order, exist := app.lru.Get(orderID)
 
+	tt := time.Now()
+
 	if !exist {
 		var err error
 
 		app.log.Info("Reading from Postgres")
 		order, err = app.conn.RequestOrder(orderID)
 		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
+			http.Error(w, err.Error(), http.StatusNotFound)
 			return
 		}
 		app.lru.Set(orderID, order)
+	} else {
+		app.log.Info("Taking from Cache")
 	}
+
+	app.log.Info("Time taken to response", slog.String("orderID", orderID), slog.Any("time", time.Since(tt)))
 
 	b, err := json.MarshalIndent(order, "", "  ")
 	if err != nil {
