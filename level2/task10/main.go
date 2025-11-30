c9cf4a5b7cc0ee6d3aadcfc25c106b0e4564c12c
Ermachkov Yaroslav <eryar.school@gmail.com>
2025-11-30 03:00:52 +0300

    task10: sort utility

---
+++ level2/task10/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"os"
+
+	flag "github.com/spf13/pflag"
+)
+
+func parsing() *Config {
+	var conf Config
+	flag.BoolVarP(&conf.reverse, "reverse", "r", false, "Sorting data in reverse order")
+	flag.BoolVarP(&conf.useDigit, "numbered", "n", false, "Interpret strings like a numbers")
+	flag.BoolVarP(&conf.unique, "unique", "u", false, "Print only unique strings in output")
+	flag.IntVarP(&conf.column, "column", "k", -1, "Compare using data as a numbers")
+	flag.BoolVarP(&conf.sorted, "check", "c", false, "Check the data sorting")
+	flag.Parse()
+	return &conf
+}
+
+func main() {
+	var sortMe Sortable
+
+	sortMe.Config = parsing()
+
+	sortMe.Fill(os.Stdin)
+
+	sortMe.Sort()
+
+	sortMe.Print()
+}
---
+++ level2/task10/sort.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+type Config struct {
+	column   int
+	reverse  bool
+	useDigit bool
+	unique   bool
+	sorted   bool
+}
+
+type Pair struct {
+	original string
+	cmpStr   string
+	cmpInt   float64
+}
+
+type Sortable struct {
+	*Config
+	a   []Pair
+	cnt int
+}
+
+func (s *Sortable) Len() int {
+	return len(s.a)
+}
+
+func (s *Sortable) Swap(i, j int) {
+	s.a[i], s.a[j] = s.a[j], s.a[i]
+	s.cnt++
+}
+
+func (s *Sortable) Less(i, j int) bool {
+	if s.useDigit {
+		if s.reverse {
+			return s.a[i].cmpInt >= s.a[j].cmpInt
+		}
+		return s.a[i].cmpInt < s.a[j].cmpInt
+	}
+
+	if s.reverse {
+		return s.a[i].cmpStr >= s.a[j].cmpStr
+	}
+	return s.a[i].cmpStr < s.a[j].cmpStr
+}
+
+func (s Sortable) Print() {
+	var lst string
+	for i, pair := range s.a {
+		if s.unique && i > 0 && lst == pair.original {
+			continue
+		}
+		fmt.Println(pair.original)
+	}
+
+	if s.sorted && s.cnt > 0 {
+		fmt.Println("Data is not sorted!")
+	}
+}
+
+func (s *Sortable) Fill(r io.Reader) {
+	scan := bufio.NewScanner(r)
+	for scan.Scan() {
+		tmp := scan.Text()
+		s.a = append(s.a, Pair{original: tmp})
+	}
+}
+
+func (s *Sortable) Sort() {
+	for i := range s.a {
+		s.a[i].cmpStr = s.a[i].original
+		if s.column >= 1 {
+			if str := strings.Fields(s.a[i].original); len(str) >= s.column {
+				s.a[i].cmpStr = str[s.column]
+			}
+		}
+		if s.useDigit {
+			if val, err := strconv.ParseFloat(s.a[i].cmpStr, 64); err == nil {
+				s.a[i].cmpInt = float64(val)
+			} else {
+				fmt.Printf("Error while parsing %d to float64 with: %s\n", i+1, err)
+			}
+		}
+	}
+	sort.Sort(s)
+}
